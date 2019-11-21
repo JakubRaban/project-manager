6116622377515d5445e61df0e7d4c5b3e0187495
Jakub <jakub.raban@gmail.com>
2019-11-21 14:03:42 +0100

    Initial configuration

---
+++ src/main/java/pl/edu/agh/gastronomiastosowana/ApplicationRunner.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.gastronomiastosowana;
+
+import javafx.application.Application;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        Application.launch(MainWindow.class, args);
+    }
+}
---
+++ src/main/java/pl/edu/agh/gastronomiastosowana/MainWindow.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.gastronomiastosowana;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
