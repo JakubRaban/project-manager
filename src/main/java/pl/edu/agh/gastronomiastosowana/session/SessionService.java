2e122f685d0cf3e852e8b63c008a5acd4bd37a4e
Jakub <jakub.raban@gmail.com>
2019-11-25 17:22:28 +0100

    Replace with HibernateSessionService

---
+++ src/main/java/pl/edu/agh/gastronomiastosowana/session/SessionService.java
@@ -0,0 +1,28 @@
+package pl.edu.agh.gastronomiastosowana.session;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionService {
+    private static final SessionFactory sessionFactory = new Configuration().configure() // configures settings from
+            // hibernate.cfg.xml
+            .buildSessionFactory();
+
+    private static Session session;
+
+    public static void openSession() {
+        session = sessionFactory.openSession();
+    }
+
+    public static Session getSession() {
+        if ( session == null ){
+            openSession();
+        }
+        return session;
+    }
+
+    public static void closeSession() {
+        session.close();
+    }
+}
