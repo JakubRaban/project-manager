8f6c3a5927ee6dbeb43e9e3fe7a5ef51841d530c
MichalGrzybek <mgrzybek@student.agh.edu.pl>
2019-12-11 17:01:34 +0100

    Added controllers

--- src/main/java/pl/edu/agh/gastronomiastosowana/controller/ProjectGroupViewController.java
+++ src/main/java/pl/edu/agh/gastronomiastosowana/controller/ProjectGroupViewController.java
@@ -5,133 +5,115 @@ import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.binding.StringBinding;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import pl.edu.agh.gastronomiastosowana.dao.ParticipantDao;
 import pl.edu.agh.gastronomiastosowana.dao.ProjectDao;
 import pl.edu.agh.gastronomiastosowana.dao.ProjectGroupDao;
+import pl.edu.agh.gastronomiastosowana.model.Participant;
 import pl.edu.agh.gastronomiastosowana.model.Project;
+import pl.edu.agh.gastronomiastosowana.model.ProjectGroup;
+import pl.edu.agh.gastronomiastosowana.model.aggregations.ProjectGroupList;
 import pl.edu.agh.gastronomiastosowana.model.aggregations.ProjectList;
 import pl.edu.agh.gastronomiastosowana.model.interactions.ItemInputType;
 import pl.edu.agh.gastronomiastosowana.presenter.ProjectEditPanePresenter;
+import pl.edu.agh.gastronomiastosowana.presenter.ProjectGroupEditPanePresenter;
+import pl.edu.agh.gastronomiastosowana.presenter.ProjectGroupParticipantEditPanePresenter;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
-public class ProjectViewController {
-    private ProjectList projectList;
+public class ProjectGroupViewController {
+
+    public Label chiefLabel;
+    private ProjectGroupList projectGroupList;
     private ProjectDao projectDao;
     private ProjectGroupDao projectGroupDao;
+    private ParticipantDao participantDao;
 
-    @FXML private TableView<Project> tableView;
+    @FXML private TableView<ProjectGroup> tableView;
 
-    @FXML private Button addNewButton;
-    @FXML private Button editButton;
-    @FXML private Button removeButton;
-
-    @FXML private Label projectGroupNameLabel;
     @FXML private Label activeLabel;
-    @FXML private Label creationDateLabel;
-    @FXML private Label chiefLabel;
-    @FXML private Label participantCountLabel;
-    @FXML private Label averageScoreLabel;
+    @FXML private Label projectNameLabel;
+    @FXML private Button editButton;
+    @FXML private Button editParticipantsButton;
 
     @FXML
     private void initialize() {
-        projectList = new ProjectList();
+        projectGroupList = new ProjectGroupList();
         projectDao = new ProjectDao();
         projectGroupDao = new ProjectGroupDao();
 
         bindTableProperties();
         bindButtonProperties();
-        bindProjectGroupProperties();
+        bindProjectProperties();
         loadAll();
     }
-
     private void bindTableProperties() {
-        tableView.itemsProperty().bind(projectList.projectsProperty());
+        tableView.itemsProperty().bind(projectGroupList.projectGroupProperty());
     }
 
     private void bindButtonProperties() {
         BooleanBinding disableBinding = tableView.getSelectionModel().selectedItemProperty().isNull();
         editButton.disableProperty().bind(disableBinding);
-        removeButton.disableProperty().bind(disableBinding);
+        editParticipantsButton.disableProperty().bind(disableBinding);
     }
 
-    private void bindProjectGroupProperties() {
+    private void bindProjectProperties() {
         // TODO: fix warnings
-        ObjectBinding<String> groupNameBinding = Bindings.select(tableView.getSelectionModel().selectedItemProperty(), "projectGroup", "groupName");
-        projectGroupNameLabel.textProperty().bind(groupNameBinding);
-
+        ObjectBinding<String> projectNameBinding = Bindings.select(tableView.getSelectionModel().selectedItemProperty(), "project", "name");
+        projectNameLabel.textProperty().bind(projectNameBinding);
+/*
         ObjectBinding<Boolean> activeBinding = Bindings.select(tableView.getSelectionModel().selectedItemProperty(), "projectGroup", "active");
         StringBinding activeStringBinding = Bindings
                 .when(activeBinding.isNotNull())
                 .then(activeBinding.asString())
                 .otherwise("");
         activeLabel.textProperty().bind(activeStringBinding);
-
+*/
+        /*
         ObjectBinding<LocalDate> creationDateBinding = Bindings.select(tableView.getSelectionModel().selectedItemProperty(), "projectGroup", "creationDate");
         StringBinding creationDateStringBinding = Bindings
                 .when(creationDateBinding.isNotNull())
                 .then(creationDateBinding.asString())
                 .otherwise("");
-        creationDateLabel.textProperty().bind(creationDateStringBinding);
+        creationDateLabel.textProperty().bind(creationDateStringBinding);*/
     }
 
     @FXML
     private void loadActive() {
-        projectList.setProjects(FXCollections.observableArrayList());
+        projectGroupList.setProjectGroups(FXCollections.observableArrayList());
     }
 
     @FXML
     private void loadAll() {
-        projectList.setProjects(FXCollections.observableList(projectDao.findAll()));
+        projectGroupList.setProjectGroups((FXCollections.observableList(projectGroupDao.findAll())));
     }
 
     @FXML
     private void loadArchival() {
-        projectList.setProjects(FXCollections.observableArrayList());
-    }
-
-    @FXML
-    void addNewProject() {
-        try {
-            Dialog editDialog = new Dialog();
-            FXMLLoader loader = new FXMLLoader();
-            Parent parent = loader.load(getClass().getResourceAsStream("/fxml/ProjectEditPane.fxml"));
-            ProjectEditPanePresenter presenter = loader.getController();
-            presenter.setItemInputType(ItemInputType.NEW_ITEM);
-            presenter.setWindow(editDialog.getDialogPane().getScene().getWindow());
-            editDialog.getDialogPane().setContent(parent);
-            editDialog.showAndWait();
-
-            if (presenter.isAccepted()) {
-                projectDao.save(presenter.getProject());
-                projectList.getProjects().add(presenter.getProject());
-            }
-        }
-        catch (IOException exc) {
-            throw new RuntimeException(exc);
-        }
+        projectGroupList.setProjectGroups(FXCollections.observableArrayList());
     }
 
     @FXML
     void editSelectedProject() {
-        Project selectedProject = tableView.getSelectionModel().getSelectedItem();
+        ProjectGroup selectedProjectGroup = tableView.getSelectionModel().getSelectedItem();
         try {
             Dialog editDialog = new Dialog();
             FXMLLoader loader = new FXMLLoader();
-            Parent parent = loader.load(getClass().getResourceAsStream("/fxml/ProjectEditPane.fxml"));
-            ProjectEditPanePresenter presenter = loader.getController();
+            Parent parent = loader.load(getClass().getResourceAsStream("/fxml/ProjectGroupEditPane.fxml"));
+            ProjectGroupEditPanePresenter presenter = loader.getController();
             presenter.setItemInputType(ItemInputType.EDIT_ITEM);
-            presenter.setProject(selectedProject);
+            presenter.setProjectGroup(selectedProjectGroup);
             presenter.setWindow(editDialog.getDialogPane().getScene().getWindow());
             editDialog.getDialogPane().setContent(parent);
             editDialog.showAndWait();
             if (presenter.isAccepted()) {
-                projectGroupDao.save(selectedProject.getProjectGroup());
-                projectDao.save(selectedProject);
+                projectGroupDao.save(selectedProjectGroup);
+                //projectDao.save(selectedProjectGroup);
             }
         }
         catch (IOException exc) {
@@ -139,10 +121,25 @@ public class ProjectViewController {
         }
     }
 
-    @FXML
-    void removeSelectedProject() {
-        Project selectedProject = tableView.getSelectionModel().getSelectedItem();
-        projectDao.delete(selectedProject);
-        projectList.getProjects().remove(selectedProject);
+    public void editParticipants(ActionEvent actionEvent) {
+        ProjectGroup selectedProjectGroup = tableView.getSelectionModel().getSelectedItem();
+        try {
+            Dialog editDialog = new Dialog();
+            FXMLLoader loader = new FXMLLoader();
+            Parent parent = loader.load(getClass().getResourceAsStream("/fxml/ProjectGroupParticipantsEditPane.fxml"));
+            ProjectGroupParticipantEditPanePresenter presenter = loader.getController();
+            presenter.setItemInputType(ItemInputType.EDIT_ITEM);
+            presenter.setProjectGroup(selectedProjectGroup);
+            presenter.setWindow(editDialog.getDialogPane().getScene().getWindow());
+            editDialog.getDialogPane().setContent(parent);
+            editDialog.showAndWait();
+            if (presenter.isAccepted()) {
+                projectGroupDao.save(selectedProjectGroup);
+                //projectDao.save(selectedProjectGroup);
+            }
+        }
+        catch (IOException exc) {
+            throw new RuntimeException(exc);
+        }
     }
 }
